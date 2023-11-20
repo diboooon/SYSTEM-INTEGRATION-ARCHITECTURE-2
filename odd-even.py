ee23c74b8fe03945db4a2a34248d96464b2310d3
diboooon <134046071+diboooon@users.noreply.github.com>
2023-11-20 15:11:25 +0800

    Create odd-even.py

---
+++ odd-even.py
@@ -0,0 +1,11 @@
+#Exercise 3 _ Odd or Even
+
+value = input ("Enter a number: ")
+if int(value) % 2 ==0:
+    print(value)  
+    print("is an even number")
+ 
+    
+else:
+    print(value) 
+    print("is an odd number")
