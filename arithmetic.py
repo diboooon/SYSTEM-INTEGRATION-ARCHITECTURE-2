30beb9770ba87a2df53a160dcbbb16e9781ba4ae
diboooon <134046071+diboooon@users.noreply.github.com>
2023-11-20 14:48:00 +0800

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+#Exercise 2 - Arithmetic Operations
+
+num1 = 20
+num2 = 30
+
+#Operations
+sum = num1 + num2
+quotient = num1 / num2 
+difference = num1 - num2 
+product = num1 * num2 
+
+#results
+print("The sum is",sum)
+print("The quotient is",quotient)
+print("The difference is",difference)
+print("The product is",product)
