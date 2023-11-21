ed42efbd4475ab1ab2a676c66680e06f1e574d3c
diboooon <134046071+diboooon@users.noreply.github.com>
2023-11-21 15:23:03 +0800

    Update odd_or_even_functions.py

--- odd_or_even_functions.py
+++ odd_or_even_functions.py
@@ -1,9 +1,9 @@
-#Exercise7: odd_or_even_functions.py
+#Exercise7: Odd_or_even_functions.py
 
 def even_or_odd():
-    value = input("What Number? ")
+    value = input("Enter a Number: ")
     if int(value) % 2 == 0:
-        print(f"{value} is even")
+        print(f"{value} is an even number")
     else:
-        print(f"{value} is odd")
+        print(f"{value} is an odd number")
 even_or_odd()
